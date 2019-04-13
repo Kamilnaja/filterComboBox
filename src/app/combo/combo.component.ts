ec676ccbae131c87e188965d14894cf1c3640134
najak <kamilnaja@gmail.com>
2019-04-13 16:15:21 +0200

    basic functionality works

--- src/app/combo/combo.component.ts
+++ src/app/combo/combo.component.ts
@@ -9,7 +9,7 @@ import { DictionaryValue } from './DictionaryValue.js';
   styleUrls: ['./combo.component.css']
 })
 
-export class ComboComponent implements OnInit, OnChanges, OnDestroy {
+export class ComboComponent implements OnInit, OnChanges {
   selectedSport = new FormControl('')
 
   public sportsList: DictionaryValue[];
@@ -29,42 +29,35 @@ export class ComboComponent implements OnInit, OnChanges, OnDestroy {
 
   ngOnInit() {
     this.sportsListsCopy = this.sportsList;
-    this.sub = this.selectedSport.valueChanges.subscribe(item => {
-      console.log(item);
-    });
   }
 
   ngOnChanges(): void {
   }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
-
-  public get sportControl(): AbstractControl {
+  public get getSelectedSport(): AbstractControl {
     return this.selectedSport;
   }
 
   public handleSubmit() {
-    console.log(this.selectedSport.value);
+    console.log(this.mapDescriptionToObject(this.getSelectedSport.value).value);
   }
 
-  public mapValueIntoObject(value: string): DictionaryValue { // todo - move into service
-    return this.sportsList.find(item => item.value === value);
+  public mapDescriptionToObject(description: string): DictionaryValue { // todo - move into service
+    return this.sportsList.find(item => item.description === description);
   }
 
   public selectItem(item: any) {
     this.selectedItem = item;
     this.isOptionVisible = false;
-    console.log(this.selectedItem);
-    // todo zapylić tym dane w formularzu
+    this.selectedSport.setValue(this.selectedItem)
   }
 
-  public toggleVisible() {
-    this.isOptionVisible = !this.isOptionVisible;
+  public toggleVisible(val: boolean) {
+    val ? this.isOptionVisible = true : this.isOptionVisible = false; // todo - refactor
   }
   // todo - akcja, gdy klikniemy gdziekolwiek poza
   // todo - gdy zjedziemy w dół z formularza, zaznacz pierwszy item
+  // todo - gdy klikniemy w item, niech wypelni on formularz
   public expandSelect(e) {
     if (e.code === 'ArrowDown') {
       console.log(e);
@@ -77,8 +70,9 @@ export class ComboComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   public filterModel(): DictionaryValue[] {
-    let val = this.selectedSport.value
-    return this.sportsListsCopy.filter((item: DictionaryValue) => item.description.startsWith(val))
+    let val = this.getSelectedSport.value
+    return this.sportsListsCopy
+      .filter((item: DictionaryValue) => item.description.toLowerCase().startsWith(val.toLowerCase()));
   }
 
   public selectDown(i: number) {
